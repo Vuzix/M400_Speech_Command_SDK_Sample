40d3f22be8589a89951b7e45cd35c1750359261a
bkeller-vuzix <98604695+bkeller-vuzix@users.noreply.github.com>
2026-02-04 09:20:13 -0500

    Updated so existing wake/voice-off phrases are not deleted.
    Deleting the existing wake and voice-off phrases is not recommended.
    This demonstrates the recommended implementation.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # M400_Speech_Command_SDK_Sample
 
 Demonstrates how an application can customize the vocabulary of the built-in speech command engine and receive voice commands.
+
+For additional information, please refer to the [Vuzix Speech SDK Javadocs](https://vuzix.github.io/sdk-speechrecognitionservice/javadoc/reference/classes.html).
+
+## Legal
+Use of the SDK is available to developers agreeing to the
+[VUZIX® SOFTWARE DEVELOPMENT KIT LICENSE AND CONFIDENTIALITY AGREEMENT](https://www.vuzix.com/pages/vuzix%C2%AE-software-development-kit-license-and-confidentiality-agreement)
--- app/src/main/java/com/vuzix/sample/vuzix_speech_recognition/VoiceCmdReceiver.java
+++ app/src/main/java/com/vuzix/sample/vuzix_speech_recognition/VoiceCmdReceiver.java
@@ -1,5 +1,5 @@
 /* 
-Copyright (c) 2017, Vuzix Corporation
+Copyright (c) 2017-2026, Vuzix Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -32,10 +32,12 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package com.vuzix.sample.vuzix_speech_recognition;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -43,23 +45,74 @@ import android.widget.Toast;
 
 import com.vuzix.sdk.speechrecognitionservice.VuzixSpeechClient;
 
+import java.util.List;
 
 /**
- * Class to encapsulate all voice commands
+ * Sample class to encapsulate all voice commands
  */
 public class VoiceCmdReceiver  extends BroadcastReceiver {
     // Voice command substitutions. These substitutions are returned when phrases are recognized.
     // This is done by registering a phrase with a substitution. This eliminates localization issues
     // and is encouraged
     final String MATCH_POPUP = "popup";
-    final String MATCH_CLEAR = "clear_substitution"; // 18 char
+    final String MATCH_CLEAR = "clear_substitution";
     final String MATCH_RESTORE = "restore";
-    final String MATCH_EDIT_TEXT = "edit_text_pressed"; // 17 char
+    final String MATCH_EDIT_TEXT = "edit_text_pressed";
 
     // Voice command custom intent names
     final String TOAST_EVENT = "other_toast";
 
     private MainActivity mMainActivity;
+    VuzixSpeechClient sc;
+    boolean originalEnabledState;
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+    private void registerIntentFilter(IntentFilter intentFilter) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            mMainActivity.registerReceiver(this, intentFilter, Context.RECEIVER_EXPORTED);
+        } else {
+            mMainActivity.registerReceiver(this, intentFilter);
+        }
+    }
+
+    /**
+     * <p>The speech SDK 1.8 and newer do <i>not</i> require this. That version was released in 2020 on
+     * M400 v1.x products. If your code is only intended to run on current Vuzix hardware such as
+     * M400, Blade2, Shield, and LX1, <u>you do not need</u> this functionality.</p>
+     *
+     * <p>This code us relevant only for M300 and original Blade.</p>
+     *
+     * <p>This mechanism is <u>strongly discouraged</u> on modern devices</p>
+     */
+    private void handleLegacyWakeWords() {
+        // Delete every phrase in the dictionary! (Available in SDK version 1.3 and newer)
+        //
+        // Note! When developing applications on the Vuzix Blade and Vuzix M400, deleting all
+        // phrases in the dictionary removes the wake-up word(s) and voice-off words. The M300
+        // cannot change the wake-up word, so "hello vuzix" is unaffected by the deletePhrase call.
+        sc.deletePhrase("*");
+
+        // For Blade and M400, the wake-word can be modified. This call has no impact on the M300,
+        // which ignores added/deleted wake words.
+        //
+        // Please always keep the wake word "hello vuzix" to prevent confusion. You can add additional
+        // wake words specific to your application by imitating the code below
+        try {
+            sc.insertWakeWordPhrase("hello vuzix");      // Add-back the default phrase for consistency (Blade and M400 only)
+        } catch (NoSuchMethodError e) {
+            Log.i(mMainActivity.LOG_TAG, "Setting wake words is not supported. It is introduced in M300 v1.6.6, Blade v2.6, and M400 v1.0.0");
+        }
+        sc.insertWakeWordPhrase("hello sample app"); // Add application specific wake-up phrase (Blade and M400 only)
+
+        try {
+            // For all platforms, the voice-off phrase can be modified
+            sc.insertVoiceOffPhrase("voice off");      // Add-back the default phrase for consistency
+            sc.insertVoiceOffPhrase("privacy please"); // Add application specific stop listening phrase
+            sc.insertVoiceOffPhrase("stop listening"); // Add application specific stop listening phrase
+        } catch (NoSuchMethodError e) {
+            Log.i(mMainActivity.LOG_TAG, "Setting voice off is not supported. It is introduced in M300 v1.6.6, Blade v2.6, and M400 v1.0.0");
+        }
+    }
 
     /**
      * Constructor which takes care of all speech recognizer registration
@@ -68,45 +121,43 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
     public VoiceCmdReceiver(MainActivity iActivity)
     {
         mMainActivity = iActivity;
-        mMainActivity.registerReceiver(this, new IntentFilter(VuzixSpeechClient.ACTION_VOICE_COMMAND));
+        registerIntentFilter(new IntentFilter(VuzixSpeechClient.ACTION_VOICE_COMMAND));
         Log.d(mMainActivity.LOG_TAG, "Connecting to Vuzix Speech SDK");
-
         try {
-            // Create a VuzixSpeechClient from the SDK
-            VuzixSpeechClient sc = new VuzixSpeechClient(iActivity);
-            // Delete specific phrases. This is useful if there are some that sound similar to yours, but
-            // you want to keep the majority of them intact
-            //sc.deletePhrase("go home");
-            //sc.deletePhrase("go back");
-
-            // Delete every phrase in the dictionary! (Available in SDK version 1.3 and newer)
-            //
-            // Note! When developing applications on the Vuzix Blade and Vuzix M400, deleting all
-            // phrases in the dictionary removes the wake-up word(s) and voice-off words. The M300
-            // cannot change the wake-up word, so "hello vuzix" is unaffected by the deletePhrase call.
-            sc.deletePhrase("*");
-
-            // For Blade and M400, the wake-word can be modified. This call has no impact on the M300,
-            // which ignores added/deleted wake words.
-            //
-            // Please always keep the wake word "hello vuzix" to prevent confusion. You can add additional
-            // wake words specific to your application by imitating the code below
+            float sdkVersion = 1.0f;
             try {
-                sc.insertWakeWordPhrase("hello vuzix");      // Add-back the default phrase for consistency (Blade and M400 only)
-                sc.insertWakeWordPhrase("hello sample app"); // Add application specific wake-up phrase (Blade and M400 only)
-            } catch (NoSuchMethodError e) {
-                Log.i(mMainActivity.LOG_TAG, "Setting wake words is not supported. It is introduced in M300 v1.6.6, Blade v2.6, and M400 v1.0.0");
+                sdkVersion = VuzixSpeechClient.getEngineVersion();
+            } catch (Exception ignored) {
+                Log.w(mMainActivity.LOG_TAG, "Unable to query speech SDK version running on the device");
             }
 
-            try {
-                // For all platforms, the voice-off phrase can be modified
-                sc.insertVoiceOffPhrase("voice off");      // Add-back the default phrase for consistency
-                sc.insertVoiceOffPhrase("privacy please"); // Add application specific stop listening phrase
-            } catch (NoSuchMethodError e) {
-                Log.i(mMainActivity.LOG_TAG, "Setting voice off is not supported. It is introduced in M300 v1.6.6, Blade v2.6, and M400 v1.0.0");
+            // Create a VuzixSpeechClient from the SDK
+            sc = new VuzixSpeechClient(iActivity);
+
+            if (sdkVersion < 1.8f) {
+                // This is not needed for most applications!
+                // Most applications will exit with an error in this case.
+                // This is for Vuzix devices with an Operating System prior to 2021.
+                handleLegacyWakeWords();
+            } else {
+                // We want to delete the entire existing base vocabulary except the wake words and
+                // voice-off words.
+                // Rather than hard-coding those, we query the current words on the current system.
+                // This returns the full list in the current language, and is very robust
+                List<String> phrasesToKeep = sc.getWakeWordPhrases();
+                phrasesToKeep.addAll( sc.getVoiceOffPhrases() );
+                sc.deleteAllPhrasesExcept(phrasesToKeep);
+
+                // NOTE: These should load string resources in a production application.
+                // Hard-coded English phrases are used here for readability.
+                sc.insertWakeWordPhrase("hello sample app"); // Add application specific wake-up phrase
+                sc.insertVoiceOffPhrase("privacy please");
+                sc.insertVoiceOffPhrase("stop listening");
             }
 
             // Now add any new strings.  If you put a substitution in the second argument, you will be passed that string instead of the full string
+            // NOTE: These should load string resources in a production application.
+            // Hard-coded English phrases are used here for readability.
             sc.insertKeycodePhrase("Alfa", KeyEvent.KEYCODE_A );
             sc.insertKeycodePhrase("Bravo", KeyEvent.KEYCODE_B);
             sc.insertKeycodePhrase("Charlie", KeyEvent.KEYCODE_C);
@@ -151,44 +202,40 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
             sc.insertIntentPhrase("canned toast", TOAST_EVENT);
 
             // Insert phrases for our broadcast handler
-            //
-            // ** NOTE **
-            // The "s:" is required in the SDK version 1.2, but is not required in the latest JAR distribution
-            // or SDK version 1.3.  But it is harmless when not required. It indicates that the recognizer is making a
-            // substitution.  When the multi-word string is matched (in any language) the associated MATCH string
-            // will be sent to the BroadcastReceiver
             sc.insertPhrase(mMainActivity.getResources().getString(R.string.btn_text_pop_up),  MATCH_POPUP);
+            // Since we have localized string resources, we demonstrate properly localing commands.
+            // This is how a production app should do all insert() calls.
             sc.insertPhrase(mMainActivity.getResources().getString(R.string.btn_text_restore), MATCH_RESTORE);
             sc.insertPhrase(mMainActivity.getResources().getString(R.string.btn_text_clear),   MATCH_CLEAR);
             sc.insertPhrase("Edit Text", MATCH_EDIT_TEXT);
 
-
             // See what we've done
             Log.i(mMainActivity.LOG_TAG, sc.dump());
 
-            // The recognizer may not yet be enabled in Settings. We can enable this directly
-            VuzixSpeechClient.EnableRecognizer(mMainActivity, true);
+            // The recognizer may not yet be enabled in Settings. We can enable this directly.
+            // Note: since this is system-wide, we will turn it off upon exiting
+            originalEnabledState = VuzixSpeechClient.isRecognizerEnabled(mMainActivity);
+            if(!originalEnabledState) {
+                VuzixSpeechClient.EnableRecognizer(mMainActivity, true);
+            }
         } catch(NoClassDefFoundError e) {
             // We get this exception if the SDK stubs against which we compiled cannot be resolved
             // at runtime. This occurs if the code is not being run on a Vuzix device supporting the voice
             // SDK
             Toast.makeText(iActivity, R.string.only_on_vuzix, Toast.LENGTH_LONG).show();
-            Log.e(mMainActivity.LOG_TAG, iActivity.getResources().getString(R.string.only_on_vuzix) );
-            Log.e(mMainActivity.LOG_TAG, e.getMessage());
-            e.printStackTrace();
+            Log.e(mMainActivity.LOG_TAG, iActivity.getResources().getString(R.string.only_on_vuzix), e );
             iActivity.finish();
         } catch (Exception e) {
-            Log.e(mMainActivity.LOG_TAG, "Error setting custom vocabulary: " + e.getMessage());
-            e.printStackTrace();
+            Log.e(mMainActivity.LOG_TAG, "Error setting custom vocabulary. ", e);
         }
     }
 
     /**
-     * All custom phrases registered with insertPhrase() are handled here.
+     * <p>All custom phrases registered with insertPhrase() are handled here.</p>
      *
-     * Custom intents may also be directed here, but this example does not demonstrate this.
+     * <p>Custom intents may also be directed here, but this example does not demonstrate this.</p>
      *
-     * Keycodes are never handled via this interface
+     * <p>Keycodes are never handled via this interface</p>
      *
      * @param context Context in which the phrase is handled
      * @param intent Intent associated with the recognized phrase
@@ -198,7 +245,7 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
         Log.e(mMainActivity.LOG_TAG, mMainActivity.getMethodName());
         // All phrases registered with insertPhrase() match ACTION_VOICE_COMMAND as do
         // recognizer status updates
-        if (intent.getAction().equals(VuzixSpeechClient.ACTION_VOICE_COMMAND)) {
+        if (VuzixSpeechClient.ACTION_VOICE_COMMAND.equals(intent.getAction())) {
             Bundle extras = intent.getExtras();
             if (extras != null) {
                 // We will determine what type of message this is based upon the extras provided
@@ -209,6 +256,9 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
                     // considered best practice
                     String phrase = intent.getStringExtra(VuzixSpeechClient.PHRASE_STRING_EXTRA);
                     Log.e(mMainActivity.LOG_TAG, mMainActivity.getMethodName() + " \"" + phrase + "\"");
+                    if(phrase == null) {
+                        return;
+                    }
                     // Determine the specific phrase that was recognized and act accordingly
                     switch (phrase) {
                         case MATCH_POPUP:
@@ -247,6 +297,8 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
      */
     public void unregister() {
         try {
+            // If the recognizer was disabled system-wide, restore that condition upon exiting
+            VuzixSpeechClient.EnableRecognizer(mMainActivity, originalEnabledState);
             mMainActivity.unregisterReceiver(this);
             Log.i(mMainActivity.LOG_TAG, "Custom vocab removed");
             mMainActivity = null;
@@ -272,5 +324,4 @@ public class VoiceCmdReceiver  extends BroadcastReceiver {
             Toast.makeText(mMainActivity, R.string.upgrade_vuzix, Toast.LENGTH_LONG).show();
         }
     }
-
 }
